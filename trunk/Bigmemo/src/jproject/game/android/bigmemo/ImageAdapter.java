34a8c42f3d30521137ecd52997dde5424270fead
jonathan.crucifix@gmail.com <jonathan.crucifix@gmail.com@ca363a8c-3287-be5d-730c-200481befa58>
2012-09-02 17:47:31 +0000

    BigMemo - Initial Commit.

---
+++ trunk/Bigmemo/src/jproject/game/android/bigmemo/ImageAdapter.java
@@ -0,0 +1,52 @@
+package jproject.game.android.bigmemo;
+
+import android.content.*;
+import android.widget.*;
+import android.view.*;
+
+public class ImageAdapter extends BaseAdapter {
+	private Context mContext;
+    private Integer[] mThumbIds = {   
+    		R.drawable.blue_dark, R.drawable.white_dark,
+    		R.drawable.yellow_dark, R.drawable.red_dark,
+    		R.drawable.brown_dark, R.drawable.pink_dark,
+    		R.drawable.grey_dark, R.drawable.orange_dark,
+    		R.drawable.green_dark
+    };
+    
+	public ImageAdapter(Context iaContext) {
+		// TODO Auto-generated constructor stub
+		mContext = iaContext;
+	}
+
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return mThumbIds.length;
+	}
+
+	public Object getItem(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public long getItemId(int arg0) {
+		// TODO Auto-generated method stub
+		return mThumbIds[arg0];
+	}
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {  // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }    
+}
